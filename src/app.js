140a046e85583359b5654b09fc53630993a0d795
Carlosgarciare2 <122974615+Carlosgarciare2@users.noreply.github.com>
2023-04-14 19:15:32 +0000

    Final excuse generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the_excuse").innerHTML = generatorexcuse();
+  });
   console.log("Hello Rigo from the console!");
+};
+
+let generatorexcuse = () => {
+  let pronun = ["A", "The"];
+  let subject = ["Jogger", "racoon", "dog", "driver", "comedian", "pincone"];
+  let action = ["took my", "threw my", "yelled at my", "stole my", "bit my"];
+  let object = ["homework", "toe", "car", "shoe"];
+  let where = ["on the street", "in my house", "in my driveway"];
+
+  let proInd = Math.floor(Math.random() * pronun.length);
+  let subInd = Math.floor(Math.random() * subject.length);
+  let actInd = Math.floor(Math.random() * action.length);
+  let objInd = Math.floor(Math.random() * object.length);
+  let whereInd = Math.floor(Math.random() * where.length);
+
+  return (
+    pronun[proInd] +
+    " " +
+    subject[subInd] +
+    " " +
+    action[actInd] +
+    " " +
+    object[objInd] +
+    " " +
+    where[whereInd] +
+    " "
+  );
 };
